65f5fadb8b5718fde629b8fa7d52f73a81efac49
Patrik Tomášik <tomasikpatrik@gmail.com>
2018-12-08 15:49:38 +0100

    Golint issues

--- errors.go
+++ errors.go
@@ -1,17 +1,21 @@
 package grupttor
 
+// DefaultError implements errors interface to consist error handling
 type DefaultError struct {
 	Message string
 }
 
+// implemented error interface
 func (de *DefaultError) Error() string {
 	return de.Message
 }
 
+// InterruptorWrongStateError wrap error of wrong internal interruptor state
 type InterruptorWrongStateError struct {
 	*DefaultError
 }
 
+// CreateInterruptorWrongStateError factory to create wrong state error, accepts error string
 func CreateInterruptorWrongStateError(message string) InterruptorWrongStateError {
 	return InterruptorWrongStateError{
 		&DefaultError{
--- types.go
+++ types.go
@@ -1,12 +1,18 @@
 package grupttor
 
+// Handler is func for specify handler interface
 type Handler func(interrupter *Grupttor)
 
+// InterruptorState represents internal state of interruptor
 type InterruptorState string
 
 const (
+	// INIT grupttor is initialized but not started
 	INIT         InterruptorState = "init"
+	// WAITING grupttor is started and waiting for signals
 	WAITING      InterruptorState = "waiting"
+	// INTERRUPTING received interruption signal and running interruption handler
 	INTERRUPTING InterruptorState = "interrupting"
+	// STOPPED received stop signal and running stop handler
 	STOPPED      InterruptorState = "stopped"
 )
